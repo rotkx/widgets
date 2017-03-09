183ed4c984b59d797412698acceaf52a23155feb
rotkx <rotkxmail@gmail.com>
2017-03-09 16:37:36 +0200

    Update sitemap1.js

--- sitemap1.js
+++ sitemap1.js
@@ -233,7 +233,7 @@ function displayToc(filter) {
    }
    tocTable += '</table>';
    if (numDisplayed == postTitle.length) {
-      var tocNote = '<span class="toc-note">Displaying all ' + postTitle.length + ' posts<br/></span>'; }
+      var tocNote = '<span class="toc-note">الكل ' + postTitle.length + ' مواضيع<br/></span>'; }
    else {
       var tocNote = '<span class="toc-note">Displaying ' + numDisplayed + ' posts labeled \'';
       tocNote += postFilter + '\' of '+ postTitle.length + ' posts total<br/></span>';
